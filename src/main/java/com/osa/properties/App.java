f40432a6c05c979484e9efff179b501aa38fce91
ibrahimmiah <ibrahimmiah787@gmail.com>
2020-11-04 19:23:48 -0500

    Login Test added

--- src/main/java/com/osa/properties/App.java
+++ src/main/java/com/osa/properties/App.java
@@ -3,5 +3,5 @@ package com.osa.properties;
 public interface App {
 	
 	public static final String BROWSER=PropertiesFileReader.getProValue("browser");
-
+	public static final String URL=PropertiesFileReader.getProValue("url");
 }
